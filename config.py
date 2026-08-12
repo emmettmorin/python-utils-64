19d5e064253bd21fdc3860e490172c73c329701a
Emil Draganov <emmettmorin457@gmail.com>
2026-08-12 17:14:16 +0300

    🐛 fix:configuration loader with defaults

--- config.py
+++ config.py
@@ -2,35 +2,27 @@ import json
 import os
 
 class ConfigLoader:
-    def __init__(self, config_file='config.json', default_config=None):
-        self.config_file = config_file
-        self.default_config = default_config if default_config else {}
-        self.config = self.load_config()
+    def __init__(self, default_config_path):
+        self.default_config = self.load_json(default_config_path)
+        self.user_config = {}
 
-    def load_config(self):
-        config = self.default_config.copy()
-        if os.path.exists(self.config_file):
-            with open(self.config_file, 'r') as file:
-                try:
-                    user_config = json.load(file)
-                    config.update(user_config)
-                except json.JSONDecodeError:
-                    print('Error: Invalid JSON in config file. Using defaults.')  
-        return config
+    def load_json(self, path):
+        with open(path, 'r') as file:
+            return json.load(file)
 
-    def get(self, key, default=None):
-        return self.config.get(key, default)
+    def load_user_config(self, user_config_path):
+        if os.path.exists(user_config_path):
+            self.user_config = self.load_json(user_config_path)
 
-    def set(self, key, value):
-        self.config[key] = value
-        self.save_config()
+    def get_config(self):
+        return {**self.default_config, **self.user_config}
 
-    def save_config(self):
-        with open(self.config_file, 'w') as file:
-            json.dump(self.config, file, indent=4)
+    def save_user_config(self, path):
+        with open(path, 'w') as file:
+            json.dump(self.user_config, file, indent=4)
 
-# Usage example
 if __name__ == '__main__':
-    defaults = {'log_level': 'INFO', 'output_dir': './logs'}
-    config_loader = ConfigLoader(default_config=defaults)
-    print(config_loader.config)
+    loader = ConfigLoader('default_config.json')
+    loader.load_user_config('user_config.json')
+    final_config = loader.get_config()
+    print(final_config)
