8ea6a985e58d1e675a4b82fe42da6828174cdddc
Emil Draganov <emmettmorin457@gmail.com>
2026-08-12 17:15:43 +0300

    🔄d load and save configuration settings

--- config.py
+++ config.py
@@ -1,28 +1,27 @@
 import json
 import os
 
-class ConfigLoader:
-    def __init__(self, default_config_path):
-        self.default_config = self.load_json(default_config_path)
-        self.user_config = {}
+class Config:
+    def __init__(self, config_file='config.json'):
+        self.config_file = config_file
+        self.settings = self.load_config()
 
-    def load_json(self, path):
-        with open(path, 'r') as file:
+    def load_config(self):
+        if not os.path.exists(self.config_file):
+            return {}
+        with open(self.config_file, 'r') as file:
             return json.load(file)
 
-    def load_user_config(self, user_config_path):
-        if os.path.exists(user_config_path):
-            self.user_config = self.load_json(user_config_path)
+    def get(self, key, default=None):
+        return self.settings.get(key, default)
 
-    def get_config(self):
-        return {**self.default_config, **self.user_config}
+    def set(self, key, value):
+        self.settings[key] = value
+        self.save_config()
 
-    def save_user_config(self, path):
-        with open(path, 'w') as file:
-            json.dump(self.user_config, file, indent=4)
+    def save_config(self):
+        with open(self.config_file, 'w') as file:
+            json.dump(self.settings, file, indent=4)
 
-if __name__ == '__main__':
-    loader = ConfigLoader('default_config.json')
-    loader.load_user_config('user_config.json')
-    final_config = loader.get_config()
-    print(final_config)
+    def __repr__(self):
+        return f"<Config settings={self.settings}>"
